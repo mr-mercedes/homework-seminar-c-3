aa79faf651b73ad057be5fc057eac8f5ed405956
Ekyd <Ekyd777@gmail.com>
2022-04-20 22:17:10 +0300

    add homework to 3 seminar

---
+++ Task21/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Задача 21: Напишите программу, которая принимает на вход координаты трех точек и находит расстояние между ними в 3D пространстве.");
+double x1 = Coordinate("x", "A");
+double y1 = Coordinate("y", "A");
+double z1 = Coordinate("z", "A");
+double x2 = Coordinate("x", "B");
+double y2 = Coordinate("y", "B");
+double z2 = Coordinate("z", "B");
+
+int Coordinate(string coorName, string pointName)
+{
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToInt16(Console.ReadLine());
+}
+double Quarter(double x)
+{
+    return x * x; 
+}
+
+double result = Math.Sqrt(Quarter(x2 - x1) + Quarter(y2 - y1) + Quarter(z2 - z1));
+
+Console.WriteLine($"Расстояние между точками {Math.Round(result, 2)}");
---
+++ Task23/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.");
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+for (int index = 1; number >= index; index ++)
+{
+    int newNumber = Cube(index);
+    Console.WriteLine($"{index} -> {newNumber}" );
+}
+
+
+
+int Cube(int index)
+{
+    return index = index * index * index; 
+}
