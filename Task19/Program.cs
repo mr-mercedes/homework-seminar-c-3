62a7d116deaa2b2856dadc4e4b3a52477b9d1a72
Ekyd <Ekyd777@gmail.com>
2022-04-20 09:24:50 +0300

    task19

---
+++ Task19/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Напишите программу которая принимает на вход пятизначное число и проверяет является ли оно палиндромом");
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int indexMin = 9999;
+int indexMax = 99999;
+
+if (number <= indexMin || number > indexMax)
+{
+    Console.WriteLine("Ошибка! Введите пятизначное число");
+    return;
+}
+
+string numberString = Convert.ToString(number);
+
+if (numberString[0] == numberString[4] || numberString[1] == numberString[3])
+{
+    Console.WriteLine($"Введенное число {number} палиндром");
+    return;
+}
+else
+Console.WriteLine($"Введенное число {number} не является палиндромом");
